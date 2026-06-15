03359cd5bd70de73488f6bc8dc6fafdb198de7c1
ShivamTaneja <shiivamtaneja@gmail.com>
2026-06-15 15:54:48 +0530

    feat: initialize project structure and implement base configuration for URL shortener service

---
+++ core/config.py
@@ -0,0 +1,5 @@
+class Settings:
+    APP_NAME = "URL Shortner"
+
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from core.config import settings
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello"}
+    return {"message": settings.APP_NAME}
